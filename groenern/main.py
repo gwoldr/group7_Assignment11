8b9e7aeb1c56de30817bebffc5f683393253eb6a
gwoldr <romangroenewold@gmail.com>
2022-11-03 19:16:34 -0400

    Fixed requests to base dictionary

--- groenern/main.py
+++ groenern/main.py
@@ -8,16 +8,9 @@ Created on Nov 3, 2022
 import json
 import requests
 
-response = requests.get('https://api.fda.gov/drug/event.json?api_key=4vbiCOIQc2fDQeSlS0kzkJV6fKZg2s6qNPiXayCk')
-json_string = response.content
-  
-parsed_json = json.loads(json_string) # Now we have a python dictionary
-  
-#print(parsed_json)
-#print(parsed_json['data'][0]['description'])
-#print(parsed_json['data'][0]['directionsInfo'])
-  
-total = int(parsed_json['total'])        # The number of parks that were returned
+URL = 'https://api.fda.gov/drug/event.json?api_key=4vbiCOIQc2fDQeSlS0kzkJV6fKZg2s6qNPiXayCk'
+data = requests.get(URL).json()
 
-for park in parsed_json['data']:
-    print (park)
+for result in data.get('results', []):
+  print(result)
+
